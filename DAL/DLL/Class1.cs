c8c6d4cfe77fdb05589788c08e606d6f9dcc4a50
Liza <miss.elizaveta.rengan@gmail.com>
2018-05-15 20:17:50 +0300

    Minimal BLL

---
+++ DAL/DLL/Class1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace BLL
+{
+    public class ClassWork // пропоную тут зробити найпростіші ф-ції, а всю перевірку накатати в WCF
+    {
+		public bool AddPerson(Person person)
+		{
+			try
+			{
+				using (AuctionContent db = new AuctionContent())
+				{
+					if (db.Persons.FirstOrDefault(elem => elem.Email == person.Email) == null)
+					{
+						db.Persons.Add(person);
+						return true;
+					}
+					return false;
+				}
+			}
+			catch(Exception ex)
+			{
+				Log.Logger(ex.Message);
+				return false;
+			}
+		}
+		public Person Authorization(string email, string password)
+		{
+			try
+			{
+				using (AuctionContent db = new AuctionContent())
+				{
+					return db.Persons.FirstOrDefault(elem => elem.Email == email && elem.Password == password);
+				}
+			}
+			catch(Exception ex)
+			{
+				Log.Logger(ex.Message);
+				return null;
+			}
+		}
+		public bool AddLot(Lot lot)
+		{
+			try
+			{
+				using (AuctionContent db = new AuctionContent())
+				{
+					db.Lots.Add(lot);
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Logger(ex.Message);
+				return false;
+			}
+		}
+		public bool Bet(LotHistory history)
+		{
+			try
+			{
+				using (AuctionContent db = new AuctionContent())
+				{
+					db.History.Add(history);
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Logger(ex.Message);
+				return false;
+			}
+		}
+		public List<Lot> OldLots()
+		{
+			try
+			{
+				using (AuctionContent db = new AuctionContent())
+				{
+					return db.Lots.Where(elem => elem.TimeFinish < DateTime.Now).ToList();
+				}
+			}
+			catch(Exception ex)
+			{
+				Log.Logger(ex.Message);
+				return null;
+			}
+		}
+		public List<Lot> FutureLots()
+		{
+			try
+			{
+				using (AuctionContent db = new AuctionContent())
+				{
+					return db.Lots.Where(elem => elem.TimeStart > DateTime.Now).ToList();
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Logger(ex.Message);
+				return null;
+			}
+		}
+		public List<Lot> NowLots()
+		{
+			try
+			{
+				using (AuctionContent db = new AuctionContent())
+				{
+					return db.Lots.Where(elem => elem.TimeStart > DateTime.Now && elem.TimeFinish < DateTime.Now).ToList();
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Logger(ex.Message);
+				return null;
+			}
+		}
+    }
+}
