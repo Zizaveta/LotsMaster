e9bca280dee9b8d923348d065778906bb3633f83
Liza <miss.elizaveta.rengan@gmail.com>
2018-05-15 19:25:25 +0300

    New classes

--- DAL/DAL/Model.cs
+++ DAL/DAL/Model.cs
@@ -1,6 +1,7 @@
 namespace DAL
 {
 	using System;
+	using System.Collections.Generic;
 	using System.Data.Entity;
 	using System.Linq;
 
@@ -10,9 +11,6 @@ namespace DAL
 			: base("name=Model")
 		{
 		}
-
-		// Add a DbSet for each entity type that you want to include in your model. For more information 
-		// on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
 		// public virtual DbSet<MyEntity> MyEntities { get; set; }
 	}
@@ -25,11 +23,26 @@ namespace DAL
 		public string Password { get; set; }
 	}
 
+	public class Lot
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string About { get; set; }
+		public int StartPrice { get; set; } // пропоную не гратися з копійками
+		public string Photo { get; set; } // тут я не вкурсі як та фотка буде закідатися від клієнта на сервер, а потім по всіх клієнтах
+		public DateTime TimeStart { get; set; }
+		public DateTime TimeFinish { get; set; }
+		public bool IsForAllNow { get; set; } // if (DateTime.Now >TimeStart && DateTime.Now < TimeFinish) return true
 
+		public Person WhoSale { get; set; }
+		public Person WhoBuy { get; set; }
+		public virtual ICollection<LotHistory> History { get; set; }
+	}
 
-	//public class MyEntity
-	//{
-	//    public int Id { get; set; }
-	//    public string Name { get; set; }
-	//}
+	public class LotHistory  // без цього не вийде обійтися 
+	{
+		public int Id { get; set; }
+		public Person Persson { get; set; }
+		public int Money { get; set; }
+	}
 }
