b759585855d0a8cacd133e74d419477578b37db9
Liza <miss.elizaveta.rengan@gmail.com>
2018-05-15 20:51:06 +0300

    Add WCF

---
+++ DAL/WCF/WCFWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using DAL;
+using BLL;
+namespace WCF
+{
+	[ServiceContract]
+	public interface AuctionClient
+	{
+		[OperationContract]
+		string AddPerson(Person person);
+		[OperationContract]
+		Person Authorization(string email, string password);
+		[OperationContract]
+		string AddLot(Lot lot);
+		[OperationContract]
+		string Bet(LotHistory history);
+		[OperationContract]
+		List<Lot> OldLots();
+		[OperationContract]
+		List<Lot> FutureLots();
+		[OperationContract]
+		List<Lot> NowLots();
+	}
+
+    public class WCFWork
+    {
+
+    }
+}
