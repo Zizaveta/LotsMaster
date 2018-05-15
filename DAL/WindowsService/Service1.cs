b309c9206beb0b3fd980b3f35ee0e6f7df89c594
Liza <miss.elizaveta.rengan@gmail.com>
2018-05-15 22:19:51 +0300

    Ready!!!

---
+++ DAL/WindowsService/Service1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using WCF;
+using DAL;
+namespace WindowsService
+{
+	public partial class Service1 : ServiceBase
+	{
+		public Service1()
+		{
+			InitializeComponent();
+		}
+		ServiceHost sh;
+		protected override void OnStart(string[] args)
+		{
+			try
+			{
+				sh = new ServiceHost(typeof(AuctionClient));
+				sh.Open();
+				Log.Logger("Host start work");
+			}
+			catch(Exception ex)
+			{
+				Log.Logger(ex.Message);
+			}
+		}
+
+		protected override void OnStop()
+		{
+			if (sh != null)
+				sh.Close();
+			Log.Logger("Host finish work");
+		}
+	}
+}
