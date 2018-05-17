1023124485152895f271cdfa75c4a4cee52f9a2d
Eulu <Lev01b27@gmail.com>
2018-05-17 16:22:33 +0300

    New style client

--- DAL/Client/MainWindow.xaml.cs
+++ DAL/Client/MainWindow.xaml.cs
@@ -13,17 +13,35 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Client.ServiceReference1;
+using System.Globalization;
 namespace Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         AuctionClientClient Client;
+        public System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        public System.Windows.Threading.DispatcherTimer LotsTimer = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            //string date1 = "6:50:14";
+            //date1, CultureInfo.CreateSpecificCulture("en-US")
+            //, CultureInfo.CreateSpecificCulture("en-US").Calendar, DateTimeKind.Utc
+            try
+            {
+                     //time = new DateTime(1,1,1,6,50,20,DateTimeKind.Local); //Work with 1,1,1 only...
+                     //TimeToEndElement.Text = time.ToShortTimeString();
+            }
+            catch (Exception ex)
+            {
+
+                Clipboard.SetText(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+            LotsTimer.Tick += new EventHandler(MainTimer);
+            LotsTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            LotsTimer.Start();
+
             //GridInfoTextBlock.Width = GridInfo.ActualWidth;
             Client = new AuctionClientClient();
             //Client.NowLots().Last().History.Last().Money;
@@ -40,6 +58,24 @@ namespace Client
             GridImage.Visibility = Visibility.Hidden;
             GridInfo.Visibility = Visibility.Visible;
         }
+        private void timerTick(object sender, EventArgs e)
+        {
+            MyCurrentPrice.Background =  new SolidColorBrush(Colors.Red);
+            System.Windows.Threading.DispatcherTimer DeleteRed = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerEnd);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 200);
+            timer.Start();
+        }
+        private void MainTimer(object sender, EventArgs e)
+        {
+            
+            //TimeToEndElement.Text = LotsTimer.
+        }
+
+        private void timerEnd(object sender, EventArgs e)
+        {
+            MyCurrentPrice.Background = new SolidColorBrush(Colors.White);
+        }
 
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
@@ -49,15 +85,19 @@ namespace Client
                 if (Convert.ToInt32(CurrentPrice.Text) < tempCountMoney)
                 //if (Client.NowLots().Last().History.Last().Money < tempCountMoney)
                 {
-                    //Треба переаисати на Ід
-                    //Client.Bet(Client.NowLots().Last().History.Last().Lot.Id, Convert.ToInt32(MyCurrentPrice.Text));
+                  //Client.Bet(Client.NowLots().Last().History.Last().Lot.Id, Convert.ToInt32(MyCurrentPrice.Text));
                     {
                         CurrentPrice.Text = tempCountMoney.ToString();
                     }
                 }
                 else
-                MessageBox.Show("<");
+                {
+                    timer.Tick += new EventHandler(timerTick);
+                    timer.Interval = new TimeSpan(0, 0, 0,0,70);
+                    timer.Start();
+                }
             }
         }
+
     }
 }
